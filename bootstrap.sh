af27c616bbafce2dc82093a75bdec989c943dd5c
Francisco Lloreda <flloreda@gmail.com>
2019-01-04 15:29:17 +0100

    Cambiamos los passwords del sistema

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,4 +13,13 @@ cd ${GITREPO}
 
 ansible-playbook -i inventory -e @myvars.yaml ansible/all.yaml
 
+echo "Cambiamos el password de root"
+passwd
+
+echo "Cambiamos el password de root"
+passwd flloreda 
+
+echo "Cambiamos el password del luks"
+cryptsetup luksChangeKey /dev/$(lsblk --fs -l | awk '/crypto_LUKS/ { print $1 }') -S 0
+
 echo "Read the README.md file to continue"
